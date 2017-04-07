761bd86bcf58b05cd01730ecd0233479012fccf0
João Batista <joao42lbatista@gmail.com>
2017-04-07 19:59:43 -0300

    clone_Slackware_multilib_JBs.sh - corrected some errors in shell script

--- clone_Slackware_multilib_JBs.sh
+++ clone_Slackware_multilib_JBs.sh
@@ -22,7 +22,7 @@
 #
 # Script: Clone some Slackware repository to a local source
 #
-# Last update: 18/01/2017
+# Last update: 07/04/2017
 #
 # Tip: Use the file inside one "old" ISO to make less things to download
 
@@ -50,14 +50,14 @@ mirrorSource="http://bear.alienbase.nl/mirrors/people/alien/multilib/"
 echo -e "$CYAN\nDefault mirror:$GREEN $mirrorSource$NC"
 
 echo -en "$CYAN\nWant change the mirror?$NC\n(y)es - (n)o $GREEN(press enter to no):$NC "
-read changeMirror
+read -r changeMirror
 
 if [ "$changeMirror" == 'y' ]; then
     mirrorSource=''
 
     while echo "$mirrorSource" | grep -v -q -E "ftp|http"; do
         echo -en "$CYAN \nType the new mirror:$NC "
-        read mirrorSource
+        read -r mirrorSource
 
         if echo "$mirrorSource" | grep -v -q -E "ftp|http"; then
             echo -e "$RED\nError: the mirror \"$mirrorSource\" is not valid.\nOne valid mirror has \"ftp\" or \"http\"$NC"
@@ -68,7 +68,7 @@ if [ "$changeMirror" == 'y' ]; then
 fi
 
 echo -en "$CYAN\nWith version Slackware you want? $GREEN(press enter to 14.2):$NC "
-read versionSlackware
+read -r versionSlackware
 
 if [ "$versionSlackware" == '' ]; then
     versionDownload="14.2"
@@ -77,7 +77,7 @@ fi
 echo -e "$CYAN\nWill download (by lftp) $GREEN\"$versionDownload\"$CYAN from $GREEN\"$mirrorSource\"$NC"
 
 echo -en "$CYAN\nWant continue? (y)es - (n)o $GREEN(press enter to yes):$NC "
-read contineLftp
+read -r contineLftp
 
 if [ "$contineLftp" == 'n' ]; then
     echo -e "$CYAN\nJust exiting by user choice$NC\n"
@@ -87,15 +87,15 @@ else
 
         echo -en "$CYAN\nDownloading$BLUE CHECKSUMS.md5$CYAN to make a$BLUE fast check$CYAN (the$BLUE local$GREEN "
         echo -e "CHECKSUMS.md5$CYAN with the$BLUE server$GREEN CHECKSUMS.md5$CYAN)$NC. Please wait...\n"
-        wget $mirrorSource/$versionDownload/CHECKSUMS.md5 -O CHECKSUMS.md5
+        wget "$mirrorSource"/"$versionDownload"/CHECKSUMS.md5 -O CHECKSUMS.md5
 
-        cd $versionDownload/
-        changeLogLocalMd5sum=`md5sum CHECKSUMS.md5`
-        cd ..
+        cd "$versionDownload" || exit
+        changeLogLocalMd5sum=$(md5sum CHECKSUMS.md5)
+        cd .. || exit
 
-        checkChangeLogMd5sum=`echo -e "$changeLogLocalMd5sum" | md5sum -c 2> /dev/null`
+        checkChangeLogMd5sum=$(echo -e "$changeLogLocalMd5sum" | md5sum -c 2> /dev/null)
 
-        changeLogMd5sumResult=`echo -e "$checkChangeLogMd5sum" | awk '{print $2}'`
+        changeLogMd5sumResult=$(echo -e "$checkChangeLogMd5sum" | awk '{print $2}')
 
         echo -en "$CYAN\nThe$BLUE CHECKSUMS.md5$CYAN in the server is"
         contineOrJump='y'
@@ -104,28 +104,28 @@ else
 
             echo -e "$GREEN equal$CYAN with the$BLUE CHECKSUMS.md5$CYAN in local folder$NC"
             echo -en "$CYAN\nWant continue/force the download or jump the download step?$NC\n(y)es to continue - (n)o to jump $GREEN(press enter to no):$NC "
-            read contineOrJump
+            read -r contineOrJump
 
         else # $changeLogMd5sumResult == FAILED
             echo -e "$RED different$CYAN from the$BLUE CHECKSUMS.md5$CYAN in local folder$NC"
             echo -en "$CYAN\nWant view the diff between these files?$NC\n(y)es - (n)o $GREEN(press enter to yes):$NC "
-            read diffChangLog
+            read -r diffChangLog
 
             if [ "$diffChangLog" != 'n' ]; then
                 echo
-                diff -u CHECKSUMS.md5 $versionDownload/CHECKSUMS.md5 
+                diff -u CHECKSUMS.md5 "$versionDownload"/CHECKSUMS.md5
             fi
             rm CHECKSUMS.md5
         fi
 
         if [ "$contineOrJump" == 'y' ]; then
-            tmpMd5sumBeforeDownload=`mktemp`
+            tmpMd5sumBeforeDownload=$(mktemp)
 
-            listOfFilesBeforeDownload=`find $versionDownload/ -type f -print`
+            listOfFilesBeforeDownload=$(find $versionDownload/ -type f -print)
 
             echo -en "$CYAN\nCreating a$BLUE md5sum$RED (before the download)$CYAN from files found (in the folder $GREEN$versionDownload/$CYAN)$NC. Please wait..."
             for file in $listOfFilesBeforeDownload; do
-                md5sum $file >> $tmpMd5sumBeforeDownload
+                md5sum "$file" >> "$tmpMd5sumBeforeDownload"
             done
             echo -e "$CYAN\n\nThe$BLUE md5sum$RED (before the download)$CYAN was saved in the tmp file: $GREEN$tmpMd5sumBeforeDownload$NC"
         fi
@@ -135,40 +135,35 @@ else
 
     if [ "$contineOrJump" == 'y' ]; then
         echo -en "$CYAN\nDownloading files$NC. Please wait...\n\n"
-
-        if [ "$downloadSource" == 'y' ]; then
-            lftp -c 'open '$mirrorSource'; mirror -c -e '$versionDownload'/'
-            # -c continue a mirror job if possible
-            # -e delete files not present at remote site
-        else
-            lftp -c 'open '$mirrorSource'; mirror -c -e --exclude source/ --exclude patches/source/ '$versionDownload'/'
-        fi
+        lftp -c 'open '"$mirrorSource"'; mirror -c -e '"$versionDownload"'/'
+        # -c continue a mirror job if possible
+        # -e delete files not present at remote site
     fi
 
     if [ "$tmpMd5sumBeforeDownload" != '' ]; then
-        tmpMd5sumAfterDownload=`mktemp`
+        tmpMd5sumAfterDownload=$(mktemp)
 
-        listOfFilesAfterDownload=`find $versionDownload/ -type f -print`
+        listOfFilesAfterDownload=$(find $versionDownload/ -type f -print)
 
         echo -en "$CYAN\nCreating a$BLUE md5sum$RED (after the download)$CYAN from files (in the folder $GREEN$versionDownload/$CYAN)$NC. Please wait..."
         for file in $listOfFilesAfterDownload; do
-            md5sum $file >> $tmpMd5sumAfterDownload
+            md5sum "$file" >> "$tmpMd5sumAfterDownload"
         done
         echo -e "$CYAN\n\nThe$BLUE md5sum$RED (after the download)$CYAN was saved in the tmp file: $GREEN$tmpMd5sumAfterDownload$NC"
 
         echo -en "$CYAN\nChecking the changes in the files$BLUE before$CYAN with$BLUE after$CYAN download$NC. Please wait..."
-        changeResult=`diff -w $tmpMd5sumBeforeDownload $tmpMd5sumAfterDownload`
+        changeResult=$(diff -w "$tmpMd5sumBeforeDownload" "$tmpMd5sumAfterDownload")
 
         if [ "$changeResult" == '' ]; then
             echo -e "$CYAN\nNone changes made in the local folder -$GREEN All file still the same after de download$NC\n"
         else
             echo -e "$RED\n\nChanges made in local files...$NC"
 
-            diffBeforeDownload=`diff -u $tmpMd5sumBeforeDownload $tmpMd5sumAfterDownload | grep -v "^--" | grep "^-" | awk '{print $2}'`
-            diffAfterDownload=`diff -u $tmpMd5sumBeforeDownload $tmpMd5sumAfterDownload | grep -v "^++" | grep "^+" | awk '{print $2}'`
+            diffBeforeDownload=$(diff -u "$tmpMd5sumBeforeDownload" "$tmpMd5sumAfterDownload" | grep -v "^--" | grep "^-" | awk '{print $2}')
+            diffAfterDownload=$(diff -u "$tmpMd5sumBeforeDownload" "$tmpMd5sumAfterDownload" | grep -v "^++" | grep "^+" | awk '{print $2}')
 
-            for lineA in `echo $diffBeforeDownload`; do
-                for lineB in `echo $diffAfterDownload`; do
+            for lineA in $diffBeforeDownload; do
+                for lineB in $diffAfterDownload; do
                     if [ "$lineA" == "$lineB" ]; then
                         filesUpdate+=$lineA\|
                     fi
@@ -180,8 +175,8 @@ else
                 echo "$filesUpdate" | sed 's/|/\n/g' | sort
             fi
 
-            for lineA in `echo $diffBeforeDownload`; do
-                diffLineDeleted=`echo $diffAfterDownload | grep $lineA`
+            for lineA in $diffBeforeDownload; do
+                diffLineDeleted=$(echo "$diffAfterDownload" | grep "$lineA")
                 if [ "$diffLineDeleted" == '' ]; then
                     filesDeleted+=$lineA\|
                 fi
@@ -192,8 +187,8 @@ else
                 echo "$filesDeleted" | sed 's/|/\n/g' | sort
             fi
 
-            for lineB in `echo $diffAfterDownload`; do
-                diffLineNewFiles=`echo $diffBeforeDownload | grep $lineB`
+            for lineB in $diffAfterDownload; do
+                diffLineNewFiles=$(echo "$diffBeforeDownload" | grep "$lineB")
                 if [ "$diffLineNewFiles" == '' ]; then
                     filesNew+=$lineB\|
                 fi
@@ -205,21 +200,17 @@ else
             fi
         fi
 
-        rm $tmpMd5sumBeforeDownload $tmpMd5sumAfterDownload
+        rm "$tmpMd5sumBeforeDownload" "$tmpMd5sumAfterDownload"
     fi
 
-    cd $versionDownload/
+    cd "$versionDownload" || exit
 
     echo -en "$CYAN\nWant check the integrity of downloaded files with the server?$NC\n(y)es - (n)o $GREEN(press enter to yes):$NC "
-    read checkFiles
+    read -r checkFiles
 
     if [ "$checkFiles" == 'y' ] || [ "$checkFiles" == '' ]; then
         echo -en "$CYAN\nChecking the integrity of the files$NC. Please wait..."
-        if [ "$downloadSource" == 'y' ]; then
-            checkFilesResult=`tail +13 CHECKSUMS.md5 | md5sum -c --quiet`
-        else
-            checkFilesResult=`tail +13 CHECKSUMS.md5 | grep -v "source/" | grep -v "patches/source/" | md5sum -c --quiet`
-        fi
+        checkFilesResult=$(tail +13 CHECKSUMS.md5 | md5sum -c --quiet)
 
         echo -en "$CYAN\n\nFiles integrity:"
         if [ "$checkFilesResult" == '' ]; then
