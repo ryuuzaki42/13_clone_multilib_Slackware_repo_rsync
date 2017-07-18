e591ff37d78730f57d812fa8fa972600c60368fa
João Batista <joao42lbatista@gmail.com>
2017-07-18 10:27:14 -0300

    testPackagesInstalled_JBs.sh - Add Script to check if all packages in a folder are installed

---
+++ testPackagesInstalled_JBs.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+#
+# Autor= João Batista Ribeiro
+# Bugs, Agradecimentos, Criticas "construtivas"
+# Mande me um e-mail. Ficarei Grato!
+# e-mail: joao42lbatista@gmail.com
+#
+# Este programa é um software livre; você pode redistribui-lo e/ou
+# modifica-lo dentro dos termos da Licença Pública Geral GNU como
+# publicada pela Fundação do Software Livre (FSF); na versão 2 da
+# Licença, ou (na sua opinião) qualquer versão.
+#
+# Este programa é distribuído na esperança que possa ser útil,
+# mas SEM NENHUMA GARANTIA; sem uma garantia implícita de ADEQUAÇÃO a
+# qualquer MERCADO ou APLICAÇÃO EM PARTICULAR.
+#
+# Veja a Licença Pública Geral GNU para mais detalhes.
+# Você deve ter recebido uma cópia da Licença Pública Geral GNU
+# junto com este programa, se não, escreva para a Fundação do Software
+#
+# Livre(FSF) Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+#
+# Script: Script to check if all packages in a folder are installed
+#
+# Last update: 18/06/2017
+#
+echo -e "\n # Script to check if all packages in a folder are installed #"
+
+folderWork=$1
+if [ "$folderWork" == '' ]; then
+    echo -e "\nError: You need pass the folder to work"
+elif [ ! -d "$folderWork" ]; then
+    echo -e "\nError: The directory \"$folderWork\" not exist"
+else
+    files=$(find "$folderWork" | grep -E "txz$|tgz$")
+
+    filesName=$(echo "$files" | rev | cut -d '.' -f2- | cut -d '/' -f1 | rev)
+
+    for pkg in $filesName; do
+        locatePkg=$(ls "/var/log/packages/$pkg" 2> /dev/null)
+
+        if [ "$locatePkg" == '' ]; then
+            echo -e "\n$pkg - not installed"
+        fi
+    done
+fi
+echo -e "\n # End of the script #\n"
