bd00e9a611e3bd868a942eef9ad0ffbab4f5be36
João Batista <joao42lbatista@gmail.com>
2018-02-07 22:43:38 -0200

    clone_Slackware_multilib_JBs.sh - corrected bug in print the files

--- clone_Slackware_multilib_JBs.sh
+++ clone_Slackware_multilib_JBs.sh
@@ -22,7 +22,7 @@
 #
 # Script: Clone some Slackware repository to a local source
 #
-# Last update: 12/01/2018
+# Last update: 07/02/2018
 #
 input1=$1
 if [ "$input1" == "noColor" ]; then
@@ -175,7 +175,7 @@ else
 
             if [ "$filesUpdate" != '' ]; then
                 echo -e "$GREEN\\nFile(s) updated:$NC"
-                echo "$filesUpdate" | sed 's/|/\\n/g' | sort
+                echo "$filesUpdate" | sed 's/|/\n/g' | sort
             fi
 
             for lineA in $diffBeforeDownload; do
@@ -187,7 +187,7 @@ else
 
             if [ "$filesDeleted" != '' ]; then
                 echo -e "$RED\\nFile(s) deleted:$NC"
-                echo "$filesDeleted" | sed 's/|/\\n/g' | sort
+                echo "$filesDeleted" | sed 's/|/\n/g' | sort
             fi
 
             for lineB in $diffAfterDownload; do
@@ -199,7 +199,7 @@ else
 
             if [ "$filesNew" != '' ]; then
                 echo -e "$BLUE\\nNew file(s) downloaded:$NC"
-                echo "$filesNew" | sed 's/|/\\n/g' | sort
+                echo "$filesNew" | sed 's/|/\n/g' | sort
             fi
         fi
 
